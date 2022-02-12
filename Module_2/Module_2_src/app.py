560db83306a620ae4487046748417d09562889d5
Cristopher Alan <50063442+CristopherA96@users.noreply.github.com>
2022-02-11 19:06:35 -0600

    Update app.py

--- Module_2/Module_2_src/app.py
+++ Module_2/Module_2_src/app.py
@@ -1,8 +1,11 @@
+# Import datetime librarie
 from datetime import *
 from dateutil.relativedelta import *
+
+# Get the date and time info by using the now() function
 now = datetime.now()
 print(now)
 
+# To the current datetime add (increase) one month, one week and 10 hours.
 now = now + relativedelta(months=1, weeks=1, hour=10)
-
 print(now)
